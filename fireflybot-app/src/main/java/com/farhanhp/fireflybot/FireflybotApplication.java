563dbf9ac98870bbf089788b781d8f1308f20c96
farhanhp <farhan.herdia123@gmail.com>
2024-04-17 22:52:42 +0700

    setup google app engine

--- fireflybot-app/src/main/java/com/farhanhp/fireflybot/FireflybotApplication.java
+++ fireflybot-app/src/main/java/com/farhanhp/fireflybot/FireflybotApplication.java
@@ -2,13 +2,28 @@ package com.farhanhp.fireflybot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FireflybotApplication {
 
+  public static final String DEFAULT_PORT_NUMBER = "8080";
+  private static final String PORT = "PORT";
 
   public static void main(String[] args) {
     SpringApplication.run(FireflybotApplication.class, args);
+  }
+
+  @Bean
+  public ReactiveWebServerFactory reactiveWebServerFactory() {
+    int port = Integer.parseInt(System.getenv().getOrDefault(PORT, DEFAULT_PORT_NUMBER));
+
+    NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
+    factory.addServerCustomizers(builder -> builder.port(port));
+
+    return factory;
   }
 
 }
